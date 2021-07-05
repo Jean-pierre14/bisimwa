424fce2394770ade0ef29bdc9c44d72588475b64
User <chiruzabisimwa@gmail.com>
2021-07-05 19:05:23 +0200

    Sql config

---
+++ system_db.sql
@@ -0,0 +1,9 @@
+-- SQL "Structure Query Language"
+CREATE DATABASE system_db;
+
+USE system_db;
+
+-- creation du table Users = utilisateurs
+CREATE TABLE Users(
+    id INT PRIMARY KEY AUTO_INCREMENT
+);
