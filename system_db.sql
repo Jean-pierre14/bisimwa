4d34249fc0a0b6ef784e3f9675850dc6479b1fa5
User <chiruzabisimwa@gmail.com>
2021-07-13 19:04:46 +0200

    Table set

--- system_db.sql
+++ system_db.sql
@@ -5,5 +5,8 @@ USE system_db;
 
 -- creation du table Users = utilisateurs
 CREATE TABLE Users(
-    id INT PRIMARY KEY AUTO_INCREMENT
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password TEXT,
 );
